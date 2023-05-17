1e40336978ba94aa1cc56b53beaf3cfb082e9414
Dawid <daw.parc@gmail.com>
2023-05-17 13:34:14 +0200

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import Header from "./components/views/Header/Header";
 import Footer from "./components/views/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <main>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ad/:id" element={<Ad />} />
+        <Route path="/ad/add" element={<AdAdd />} />
+        <Route path="/ad/edit/:id" element={<AdEdit />} />
+        <Route path="/ad/delete/:id" element={<AdDelete />} />
+        <Route path="/ad/search/:searchPhrase" element={<Search />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
-    </>
+    </main>
   );
 }
 
