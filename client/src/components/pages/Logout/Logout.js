8d25e948937dcb9eb5e32dea8c73573bc643e38d
Dawid <daw.parc@gmail.com>
2023-05-19 13:19:46 +0200

    Add logout alert

--- client/src/components/pages/Logout/Logout.js
+++ client/src/components/pages/Logout/Logout.js
@@ -2,10 +2,13 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { API_URL } from "../../../config";
 import { logOut } from "../../../redux/usersRedux";
+import { useNavigate } from "react-router-dom";
+import { Alert, Col } from "react-bootstrap";
 
 const Logout = () => {
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const options = {
@@ -15,10 +18,22 @@ const Logout = () => {
     fetch(`${API_URL}/logout`, options)
       .then(() => {
         dispatch(logOut());
+        setTimeout(() => {
+          navigate("/");
+        }, 2000);
       });
-  }, [dispatch]);
+  }, [dispatch, navigate]);
 
-  return null;
+  return (
+    <div className="my-5 d-flex justify-content-center">
+      <Col xs={12} md={6} lg={4}>
+        <Alert variant="success" className="text-center">
+          <Alert.Heading>Success!</Alert.Heading>
+          <p>You have been successfully logged out</p>
+        </Alert>
+      </Col>
+    </div>
+  );
 };
 
 export default Logout;
